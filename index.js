e29058ee4e1899ee622f88f687cf0ea1fca5c862
Andrea Hergert <ahergert24@hotmail.com>
2022-10-18 20:47:29 -0700

    created consts for files to get started

---
+++ index.js
@@ -0,0 +1,4 @@
+const Employee = require('./lib/employee');
+const Engineer = require('./lib/engineer');
+const Intern = require('./lib/intern');
+const Manager = require('./lib/manager');
---
+++ tests/engineer.test.js
@@ -0,0 +1,1 @@
+const Engineer = require("../lib/engineer");
