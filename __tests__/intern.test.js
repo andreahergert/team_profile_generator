8ce8be76359cb5ea81478510063beabe0a5e81c1
Andrea Hergert <ahergert24@hotmail.com>
2022-10-22 17:26:55 -0700

    fixed const int all tests passed

--- __tests__/intern.test.js
+++ __tests__/intern.test.js
@@ -1,31 +1,31 @@
 const Intern = require("../lib/intern");
 
 describe('Intern', () => {
-    const engr = new Intern('Name', 1, 'test@testing.com', 'school');
+    const int = new Intern('Name', 1, 'test@testing.com', 'school');
         it ('should create an object', () =>
-            expect(typeof(engr)).toEqual('object'));
+            expect(typeof(int)).toEqual('object'));
 
         it('should assign a name property', () => 
-            expect(engr.name).toEqual('Name'));
+            expect(int.name).toEqual('Name'));
 
         it('should assign an id property', () => 
-            expect(engr.id).toEqual(1));
+            expect(int.id).toEqual(1));
 
         it('should assign an email property', () => 
-            expect(engr.email).toEqual('test@testing.com'));
+            expect(int.email).toEqual('test@testing.com'));
 
         it('should have a method to getName', () => 
-            expect(engr.getName()).toEqual('Name'));
+            expect(int.getName()).toEqual('Name'));
 
         it('should have a method to getId', () => 
-            expect(engr.getId()).toEqual(1));
+            expect(int.getId()).toEqual(1));
 
         it('should have a method to getEmail', () => 
-            expect(engr.getEmail()).toEqual('test@testing.com'));
+            expect(int.getEmail()).toEqual('test@testing.com'));
 
         it('should have a method to getRole', () => 
-            expect(engr.getRole()).toEqual('Intern'));
+            expect(int.getRole()).toEqual('Intern'));
 
         it('should have a method to getSchool', () => 
-            expect(engr.getSchool()).toEqual('school'));
+            expect(int.getSchool()).toEqual('school'));
 });
